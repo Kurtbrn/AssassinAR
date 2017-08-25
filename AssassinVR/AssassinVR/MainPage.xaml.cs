e75782164ae8016c7e4fec3ec3f38cae2059a961
Maxwell Francis <maxfrancis212@gmail.com>
2017-08-26 10:27:14 +1200

    Commit message

--- AssassinVR/AssassinVR/MainPage.xaml.cs
+++ AssassinVR/AssassinVR/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
@@ -12,26 +13,20 @@ namespace AssassinVR
     {
         public MainPage()
         {
-            var button = new Button();
 
+
+            var myButton = new Button();
+            myButton.Text = "Start Assassination";
             var map = new Map(
             MapSpan.FromCenterAndRadius(
                     new Position(37, -122), Distance.FromMiles(0.3)))
             {
                 IsShowingUser = true,
-                HeightRequest = 100,
-                WidthRequest = 960,
                 VerticalOptions = LayoutOptions.FillAndExpand
             };
-            var slider = new Slider(1, 18, 1);
-            slider.ValueChanged += (sender, e) => {
-                var zoomLevel = e.NewValue; // between 1 and 18
-                var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
-                map.MoveToRegion(new MapSpan(map.VisibleRegion.Center, latlongdegrees, latlongdegrees));
-            };
-
             var stack = new StackLayout { Spacing = 0 };
             stack.Children.Add(map);
+            stack.Children.Add(myButton);
             Content = stack;
         }
     }
